c46e5dc63299f9e57f765f139c326b458b89b6cf
Samuel Foucher <samuel.foucher@gmail.com>
2020-09-29 12:14:49 -0400

    Update util.py
    removing a few functions

--- ginmodelrepo/util.py
+++ ginmodelrepo/util.py
@@ -69,80 +69,6 @@ def update_class_mapping(class_mappings, model_task):
     setattr(model_task, "_class_names", [str(_map[0]) for _map in model_outputs_sorted_by_index])
     return model_task
 
-def validate_model(model_data):
-    # type: (CkptData) -> Tuple[bool, Optional[Exception]]
-    """
-    Accomplishes required model checkpoint validation to restrict unexpected behaviour during other function calls.
-
-    All security checks or alternative behaviours allowed by native :mod:`thelper` library but that should be forbidden
-    within this API for process execution should be done here.
-
-    :param model_data: model checkpoint data with configuration parameters (typically loaded by :func:`load_model`)
-    :return: tuple of (success, exception) accordingly
-    :raises: None (nothrow)
-    """
-    model_task = model_data.get("task")
-    if isinstance(model_task, dict):
-        model_type = model_task.get("type")
-        if not isinstance(model_type, six.string_types):
-            print(f"Model task: [{model_type!s}]")
-            print(
-                f"Forbidden model checkpoint task defines unknown operation: [{model_type!s}]"
-            )
-            return False
-        model_params = model_task.get("params")
-        if not isinstance(model_params, dict):
-            print(f"Model task: [{model_params!s}]")
-            print(
-                "Forbidden model checkpoint task missing JSON definition of parameter section."
-            )
-            return False
-        model_classes = model_params.get("class_names")
-        if not (isinstance(model_classes, list) and all([isinstance(c, (int, str)) for c in model_classes])):
-            print(f"Model task: [{model_classes!s}]")
-            print(
-                "Forbidden model checkpoint task contains invalid JSON class names parameter section."
-            )
-            return False
-    elif isinstance(model_task, thelper.tasks.Task):
-        model_type = fully_qualified_name(model_task)
-        if model_type not in MODEL_TASK_MAPPING:
-            print(f"Model task: [{model_type!s}]")
-            print(
-                f"Forbidden model checkpoint task defines unknown operation: [{model_type!s}]"
-            )
-            return False
-    else:
-        # thelper security risk, refuse literal string definition of task loaded by eval() unless it can be validated
-        print(f"Model task not defined as dictionary nor `thelper.task.Task` class: [{model_task!s}]")
-        if not (isinstance(model_task, str) and model_task.startswith("thelper.task")):
-            return False, print(
-                "Forbidden model checkpoint task definition as string doesn't refer to a `thelper.task`."
-            )
-        model_task_cls = model_task.split("(")[0]
-        print(f"Verifying model task as string: {model_task_cls!s}")
-        model_task_cls = thelper.utils.import_class(model_task_cls)
-        if not (isclass(model_task_cls) and issubclass(model_task_cls, thelper.tasks.Task)):
-            print(
-                "Forbidden model checkpoint task definition as string is not a known `thelper.task`."
-            )
-            return False
-        if model_task.count("(") != 1 or model_task.count(")") != 1:
-            print(
-                "Forbidden model checkpoint task definition as string has unexpected syntax."
-            )
-            return False
-        print("Model task defined as string allowed after basic validation.")
-        try:
-            fix_str_model_task(model_task)  # attempt update but don't actually apply it
-        except ValueError:
-            print(
-                "Forbidden model checkpoint task defined as string doesn't respect expected syntax."
-            )
-            return False
-        print("Model task as string validated with successful parameter conversion")
-    return True, None
-
 def fully_qualified_name(obj):
     # type: (Union[Any, Type[Any]]) -> AnyStr
     """Obtains the ``'<module>.<name>'`` full path definition of the object to allow finding and importing it."""
@@ -185,35 +111,6 @@ def fix_str_model_task(model_task):
         raise   # failing ast converting raises ValueError
     except Exception as exc:
         raise ValueError(f"Failed literal string parsing for model task, exception: [{exc!s}]")
-
-def load_model(model_file):
-    # type: (Union[Any, AnyStr]) -> Tuple[bool, CkptData, Optional[BytesIO], Optional[Exception]]
-    """
-    Tries to load a model checkpoint file from the file-like object, file path or URL.
-
-    :return: tuple of (success, data, buffer, exception) accordingly.
-    :raises: None (nothrow)
-    """
-    try:
-        model_buffer = model_file
-        if isinstance(model_file, six.string_types):
-            if urlparse(model_file).scheme:
-                no_ssl = ssl.create_default_context()
-                no_ssl.check_hostname = False
-                no_ssl.verify_mode = ssl.CERT_NONE
-                url_buffer = urlopen(model_file, context=no_ssl)
-                model_buffer = BytesIO(url_buffer.read())
-            else:
-                with open(model_file, 'rb') as f:
-                    model_buffer = BytesIO(f.read())
-        thelper.utils.bypass_queries = True     # avoid blocking ui query
-        model_checkpoint_info = thelper.utils.load_checkpoint(model_buffer)
-    except Exception as ex:
-        return False, {}, None, ex
-    if model_checkpoint_info:
-        return True, model_checkpoint_info, model_buffer, None
-    return False, {}, None, None
-
 
 def maybe_download_and_extract(file_id, dest_path ):
     filename = dest_path.split('/')[-1]
